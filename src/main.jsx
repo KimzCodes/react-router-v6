20c8cbbab17179c35e459e9ede8a586d2dbbdd1b
kareem nour <kimzcodes@gmail.com>
2022-09-24 13:41:05 -0700

    final

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,86 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  redirect,
+} from "react-router-dom";
+
+import {
+  getContacts,
+  createContact,
+  getContact,
+  updateContact,
+  deleteContact,
+} from "./contacts";
 
 import "./index.css";
 
-import Root, {
-  loader as rootLoader,
-  action as rootAction,
-} from "./routes/Root";
+import Layout from "./routes/Layout";
+import Index from "./routes/Index";
 import ErrorPage from "./routes/ErrorPage";
 import Contact from "./routes/Contact";
-import Layout from "./routes/Layoyt";
+import ContactEdit from "./routes/ContactEdit";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Root />,
+    element: <Layout />,
     errorElement: <ErrorPage />,
-    loader: rootLoader,
-    action: rootAction,
+    loader: async () => {
+      const contacts = await getContacts();
+
+      return { contacts };
+    },
+    action: async () => {
+      await createContact();
+    },
     children: [
       {
-        path: "contacts/:contactId",
-        element: <Contact />,
+        errorElement: <ErrorPage />,
+        children: [
+          { index: true, element: <Index /> },
+          {
+            path: "contacts/:contactId",
+            element: <Contact />,
+            loader: async ({ params }) => {
+              const contact = await getContact(params.contactId);
+              if (!contact) {
+                throw new Response("", {
+                  status: 404,
+                  statusText: "Not Found",
+                });
+              }
+              return contact;
+            },
+            action: async ({ request, params }) => {
+              let formData = await request.formData();
+              return updateContact(params.contactId, {
+                favorite: formData.get("favorite") === "true",
+              });
+            },
+          },
+          {
+            path: "contacts/:contactId/update",
+            element: <ContactEdit />,
+            loader: async ({ params }) => {
+              return getContact(params.contactId);
+            },
+            action: async ({ request, params }) => {
+              const formData = await request.formData();
+              const updates = Object.fromEntries(formData);
+              await updateContact(params.contactId, updates);
+              return redirect(`/contacts/${params.contactId}`);
+            },
+          },
+          {
+            path: "contacts/:contactId/destroy",
+            action: async ({ params }) => {
+              await deleteContact(params.contactId);
+              return redirect("/");
+            },
+          },
+        ],
       },
     ],
   },
